bdfadb6c98f80eddf5a916a33ce52e445562961e
alejandro <alejosamp@gmail.com>
2023-05-04 10:50:43 -0500

    sql DB integration

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 import express from "express";
-import userRoutes from "./routes/userRoutes.js"
+import userRoutes from "./routes/userRoutes.js";
+import db from './config/db.js';
 
 const app = express();
+try {
+    await db.authenticate();
+    console.log('conexión exitosa a la db')
+} catch (error) {
+    console.log(error)
+}
 
 app.use('/auth', userRoutes);
 
@@ -12,5 +19,5 @@ app.use(express.static('public'));
 
 const port = 3000;
 app.listen(port, () => {
-    console.log("server running")
+    console.log("server running in port 3000")
 });
